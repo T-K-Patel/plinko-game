c4e9e5dd6638668e6727dc6402f82da35e9f2bc6
Tirth Kapadi <mr.t.k.patel@gmail.com>
2024-05-29 22:35:35 +0530

    Fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import express from 'express';
 import { data } from './data.js';
+import fs from 'fs';
+import path from 'path';
 const MULTIPLIER = [16, 9, 2, 1.4, 1.4, 1.2, 1.1, 1, 0.5, 1, 1.1, 1.2, 1.4, 1.4, 2, 9, 16]
 const app = express();
 
 app.set('json spaces', 4);
-app.use(express.static('public'));
+// app.use(express.static('public'));
+
+app.use((req, res, next) => {
+    try {
+        const publicFolderPath = path.join(process.cwd(), 'public');
+        const filePath = path.join(publicFolderPath, req.path);
+        if (fs.existsSync(filePath) && fs.statSync(filePath).isFile()) {
+            // File exists in public folder
+            res.sendFile(req.path, { root: './public' });
+        } else {
+            console.log("file does not exist\n" + filePath)
+            next();
+        }
+    } catch (e) {
+        console.log(e)
+        next()
+    }
+})
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
